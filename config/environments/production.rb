67f3bec92543d288a2a85ed9d994ae465f2ddf53
Alex Poon <alex@alexpoon.me>
2012-03-25 14:18:43 -0400

    mailgun login

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,13 +70,13 @@ Linkful::Application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.sendgrid.net",
+    address: "smtp.mailgun.org",
     port: 587,
     domain: "linkfu.herokuapp.com",
     authentication: :plain,
     enable_starttls_auto: true,
-    user_name: ENV["SENDGRID_USERNAME"],
-    password: ENV["SENDGRID_PASSWORD"]
+    user_name: ENV["MAILGUN_SMTP_LOGIN"],
+    password: ENV["MAILGUN_SMTP_PASSWORD"]
   }
 
 
