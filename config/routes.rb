5a438ede2272c5897e07f6d8a4b52aff2d06ec0c
Alex Poon <alex@alexpoon.me>
2012-03-25 09:20:47 -0400

    fucking bookmark path

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Linkful::Application.routes.draw do
   match '/links/create' => 'links#create'
 
   match 'bookmarklet/:userid' => 'links#bookmarklet'
-  match 'bookmark' => 'home#bookmark', as: :bookmark_path
+  match 'bookmark' => 'home#bookmark', :as => :bookmark
 
   authenticated :user do
     root :to => 'home#index'
