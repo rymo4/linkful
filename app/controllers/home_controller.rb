d1144d497b04e8317aeeb1b229c68bb7be828b0a
Ryder Moody <rydthemoodster@gmail.com>
2012-05-25 00:16:59 -0400

    style and random link

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,14 @@ class HomeController < ApplicationController
       redirect_to links_path
     end
     @users = User.where(:isTemp => false)
+    n = Link.all.count
+    @random = (0..Link.count-1).sort_by{rand}.slice(0, n).collect! do |i| 
+      Link.skip(i).first 
+    end
+    @random = @random.first
+    titles = ["land mine?", "take a risk", "im feeling lucky"]
+    @title = titles[rand titles.length]
+    
   end
 
   def bookmark
