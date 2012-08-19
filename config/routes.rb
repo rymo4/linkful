e809c76a5107871b0073463482a8949e22052cc4
Alex Poon <alex@alexpoon.me>
2012-08-19 14:12:51 -0400

    working iframe sending links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Linkful::Application.routes.draw do
   resources :links
+  resources :bookmarklinks
 
   match '/links/create' => 'links#create'
 
   match 'bookmarklet/:userid' => 'links#bookmarklet'
+  match 'bookmarklet_iframe/:userid' => 'links#bookmarklet_iframe'
   match 'bookmark' => 'home#bookmark', :as => :bookmark
 
   authenticated :user do
